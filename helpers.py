416a43c700206f3da179c0289616b73960979ce4
herrerafuente <pablo.herrefu@gmail.com>
2025-03-11 15:41:51 +0100

    creating the dice-me project and repository

---
+++ helpers.py
@@ -0,0 +1,4 @@
+import os
+
+def get_current_directory():
+    return os.getcwd()
---
+++ main.py
@@ -0,0 +1,4 @@
+from helpers import get_current_directory
+
+if __name__ == '__main__':
+    print(get_current_directory())
