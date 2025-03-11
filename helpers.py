1cc80a67e87cb2b092ecc112502b0ef0f12215c4
herrerafuente <pablo.herrefu@gmail.com>
2025-03-11 16:10:59 +0100

    helpers.py and requirements.txt files updated, installation of dependencies

--- helpers.py
+++ helpers.py
@@ -1,4 +1,27 @@
+# region Import Libraries
 import os
+import glob
+#endregion
 
+# region Helper Functions
 def get_current_directory():
     return os.getcwd()
+
+def find_photos(current_working_directory = get_current_directory()):
+    image_extensions = ('*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff')
+
+    photos = []
+    for ext in image_extensions:
+        photos.extend(glob.glob(os.path.join(current_working_directory, ext)))
+
+    if not photos:
+        return "No photos found."
+    elif len(photos) > 1:
+        return "More than one photo found."
+    else:
+        return photos
+
+
+
+
+#endregion
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from helpers import get_current_directory
 
-if __name__ == '__main__':
+def main():
     print(get_current_directory())
+
+if __name__ == '__main__':
+    main()
