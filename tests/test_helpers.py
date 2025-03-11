f7d4137c431ff94f0fbb641978af218517504d9c
herrerafuente <pablo.herrefu@gmail.com>
2025-03-11 17:16:37 +0100

    creation tests directory, test_helpers.py test cases and addition of some resources for testing, missing 3 tests, find them in TODO

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,26 +1,46 @@
 # region Import Libraries
 import unittest
 import os
+import shutil
 import tempfile
 from unittest.mock import patch
 import cv2
-import numpy as np
 # endregion
 
 # region Import the functions to be tested
 from helpers import get_current_directory, find_photos, remove_background
 # endregion
 
-def add(a, b):
-    return a + b
-
 # Test case
-class TestMathFunctions(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)  # Check if 2+3 = 5
-        self.assertEqual(add(-1, 1), 0)  # Check if -1+1 = 0
+class TestPhotoFunctions(unittest.TestCase):
+    def setUp(self):
+        #Set up a temporary directory
+        self.test_dir = tempfile.TemporaryDirectory()
+
+        self.real_image_path = r'C:\Users\pablo\PycharmProjects\dice-me\tests\resources\test_image_1.jpg'
+        self.image1 = os.path.join(self.test_dir.name, 'test1.jpg')
+        self.image2 = os.path.join(self.test_dir.name, 'test2.png')
+
+        shutil.copy(self.real_image_path, self.image1)
+        shutil.copy(self.real_image_path, self.image2)
+
+    def tearDown(self):
+        self.test_dir.cleanup()
+
+    def test_get_current_directory(self):
+        self.assertEqual(get_current_directory(), os.getcwd())
+
+    def test_find_no_photos(self):
+        empty_dir = tempfile.TemporaryDirectory()
+        photos = find_photos(empty_dir.name)
+        self.assertEqual(photos, [])
+        empty_dir.cleanup()
+
+    #TODO: Write test cases for find_photos
+    #TODO Write test cases for remove_background
 
 
+# endregion
 
 
 if __name__ == '__main__':
